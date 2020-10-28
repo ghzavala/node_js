c2b20f65e7a035d251a65777d8b59287fc5569fb
ghzavala <guille.h.zavala@gmail.com>
2020-10-28 17:56:33 -0300

    Añadiendo verificacion de datos ingresados

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// TAREA: agregar Apellido y casilla para archivo. y que funcione joi
+
+
 require("dotenv").config()
 const express = require("express")
 const app = express()
 const nodemailer = require("nodemailer")
+const joi = require("joi")
 
 const port = 1000   //mas allá del 1000 usualmente están disponibles
 
@@ -14,15 +18,28 @@ const miniOutlook = nodemailer.createTransport({
     }
 });
 
-const miniGmail = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    auth: {
-        user: 'ghz.metrogas@gmail.com',
-        pass: 'metrogas'
-    }
-});
+const schema = joi.object({
+    nombre : joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    
+    apellido : joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    
+    correo : joi.string()
+        .email({minDomainSegments : 2, tlds : { allow : ["com", "net", "tech"]}})
+        .required(),
 
+    asunto : joi.number()
+        .integer()
+        .required(),
+
+    mensaje : joi.string()
+    .required()
+})
 
 app.listen( port )
 app.use( express.static("public") ) // las configuraciones que le damos
@@ -37,15 +54,23 @@ app.TIPO_HTTP("/RUTA", (req, res) => { LO QUE TIENE QUE HACER})
 app.post("/enviar", (req, res) => {
     const contacto = req.body
 
-    miniOutlook.sendMail({
-        from: contacto.correo, // sender address
-        to: process.env.USER, // list of receivers
-        replyTo: contacto.correo, // dirección para responder el correo
-        subject: `Asunto #${contacto.asunto}`, // Subject line
-        html: `<blockquote>${contacto.mensaje}</blockquote>`, // html body
-    });
+    const validate = schema.validate( contacto )
+    
+    if( validate.error === undefined ){
+        
+        miniOutlook.sendMail({
+            from: contacto.correo, // sender address
+            to: process.env.USER, // list of receivers
+            replyTo: contacto.correo, // dirección para responder el correo
+            subject: `Asunto #${contacto.asunto}`, // Subject line
+            html: `<blockquote>${contacto.mensaje}</blockquote>`, // html body
+        });
 
-    console.log(process.env.USER)
+        res.end("Ahora vamos a enviar un email de contacto :O")
+        
+    } else {
 
-    res.end("Desde acá vamos a enviar un email de contacto :O")
+        res.end(`El ${validate.error.details[0].context.key} ingresado no es correcto. Por favor verifique los datos.`)
+        
+    }
 })
--- test.js
+++ test.js
@@ -1,5 +1,69 @@
+/*
 require("dotenv").config()
 
 console.log(process.env.USER)
 console.log(process.env.PASS)
 console.log(process.env.PORT)
+*/
+
+const joi = require("joi")
+
+const datos = {
+    nombre : "Guillermo",
+    apellido : "Zavala",
+    email : "hola@gmail.tech",
+    asunto : 1,
+    mensaje : "Esto es una prueba"
+}
+
+const isUndefined = (valor) => {
+
+}
+
+const schema = joi.object({
+    nombre : joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    
+    apellido : joi.string()
+        .min(3)    
+        .max(30)
+        .required(),
+    
+    email : joi.string()
+        .email({minDomainSegments : 2, tlds : { allow : ["com", "net", "tech"]}})
+        .required(),
+
+    asunto : joi.number()
+        .integer()
+        .required(),
+
+    mensaje : joi.string()
+    .required()
+})
+
+let valor = {}
+
+const valorA = schema.validate( datos )
+
+//console.log(valorA.error.details[0].context.key)
+
+if( valorA.error === undefined ){
+    console.log("validacion ok")
+} else {
+    console.log(`El ${valorA.error.details[0].context.key} ingresado no es válido. Por favor verifique los datos.`)
+}
+
+
+/*
+joi.validate(datos, schema, (err, value) => {
+    const id = Math.ceil(Math.random() * 9999999);
+
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("OK!!!")
+    }
+})
+*/
